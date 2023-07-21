e904f925c16b37066ccd79a34162f31453617442
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-07-21 11:58:14 +0900

    02_01の方やっちゃった

--- Function.h
+++ Function.h
@@ -173,9 +173,11 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMa
 
 struct Sphere
 {
-	Vector3 center;
-	float radius;
+	Vector3 center; //!< 中心点
+	float radius; //!< 半径
 };
+
+bool IsCollision(const Sphere& s1, const Sphere& s2);
 
 void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
 
